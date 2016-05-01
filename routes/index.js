ffef6b77066ce1899b9eae1df72bbeb9c3829273
ryanburgess <ryanb@netflix.com>
2016-05-01 16:26:22 -0700

    update desc

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ exports.index = function(req, res){
   res.render('index', { 
     title: 'Ryan Burgess',
     description: 'Ryan Burgess is a Manager, UI Engineering at Netflix in California.',
-    about: ['Ryan Burgess is a Manager, UI Engineering at Netflix in California. Throughout his years of experience he has obtained a great deal of knowledge working for internal and external clients delivering web and mobile applications. He has a extensive experience working with HTML5, CSS3, SASS, LESS, Responsive Design, JQuery, JavaScript, AngularJS, BackboneJS, NodeJS, Mobile, PHP, MySQL, ActionScript 3 and the Adobe Creative Suite.'],
+    about: ['Ryan Burgess is a Manager, UI Engineering at Netflix in California. Throughout his years of experience he has obtained a great deal of knowledge working for internal and external clients delivering web and mobile applications. He has a extensive experience working with HTML5, CSS3, SASS, LESS, Responsive Design, JavaScript, NodeJS, ReactJS, AngularJS, BackboneJS, JQuery, Mobile, PHP, MySQL, ActionScript 3 and the Adobe Creative Suite.'],
     social:[
       {
         'title': 'Github',
