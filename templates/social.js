2e8c699375be5cb6fd204065033f5174bc69e900
ryanburgess <ryanb@netflix.com>
2016-05-05 11:53:15 -0700

    start static build of site

---
+++ templates/social.js
@@ -0,0 +1,17 @@
+// Main Template
+const mainContent = require('../content/main.json');
+
+module.exports = function social() {
+  'use strict';
+  const social = mainContent[0].social;
+  let content = '';
+
+  for (const socialLinks of social) {
+    console.log(socialLinks.title)
+    content += `<li><a href="${socialLinks.url}" class="${socialLinks.class}">${socialLinks.title}</a></li>`;
+  }
+
+  return `<ul class="social">
+            ${content}
+          </ul>`;
+};
